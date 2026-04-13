cb423801fda17f630328cad0c6e6a64272edc660
oksentiukpn <oksentiuk.pn@ucu.edu.ua>
2026-04-13 13:20:42 +0300

    67 web design

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 
 
 def create_app():
@@ -6,6 +6,6 @@ def create_app():
 
     @app.route("/")
     def home():
-        return "<h1 style='background: linear-gradient(45deg, #FEDCBA, #ABCDEF); color: black;'>6 7</h1>"
+        return render_template("home.html")
 
     return app
